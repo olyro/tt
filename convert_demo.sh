1f715a71af7ef2837083144d4cdde46dd9bcc427
Christoph Müller <mueller@olyro.de>
2025-08-05 17:19:36 +0200

    new demo svg

--- convert_demo.sh
+++ convert_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-NODE_OPTIONS="--max-old-space-size=10000" svg-term --cast=$1 --out demo.svg --window
+NODE_OPTIONS="--max-old-space-size=10000" svg-term --cast=$1 --out demo.svg --window --term iterm2 --profile "./iTerm2 Solarized Light.itermcolors"
